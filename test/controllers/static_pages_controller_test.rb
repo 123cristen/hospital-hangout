783331415a8f6b5aa086bfd62553792010d2f402
Cristen Anderson <1cristen1@gmail.com>
2015-08-18 13:56:28 +0200

    Update static page titles and refactor layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Hospital Hangout"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Hospital Hangout"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Hospital Hangout"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Hospital Hangout"
   end
 
 end
