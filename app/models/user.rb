b30018b3bf9ef6c6629d8e1295fa3db49f5f84e8
Cristen Anderson <1cristen1@gmail.com>
2015-09-09 18:11:52 +0200

    Test login using user fixture

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,11 @@ class User < ActiveRecord::Base
       errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one digit"
     end
   end
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
