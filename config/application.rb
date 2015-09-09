1de4752bc09c84475a84d33b01b1c3ad509a8234
Cristen Anderson <1cristen1@gmail.com>
2015-09-09 15:42:17 +0200

    Fix form styling and other styling bugs

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,15 @@ module HospitalHangouts
     
     # Change default error field html
     # config.action_view.field_error_proc Proc.new {|html, instance| html}
-    config.action_view.field_error_proc Proc.new {|html, instance| html}
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      class_attr_index = html_tag.index 'class="'
+
+      if class_attr_index
+        html_tag.insert class_attr_index+7, 'error '
+      else
+        html_tag.insert html_tag.index('>'), ' class="error"'
+      end
+    end
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- lib/form_builder.rb
+++ lib/form_builder.rb
@@ -22,38 +22,48 @@ class FormBuilder < ActionView::Helpers::FormBuilder
 
 	# Metaprogramming
 	# Implements formatting for each of these fields instead of copy/pasting for each
-	%w(email_field text_field password_field).each do |form_method|
-		define_method(form_method) do |*args|
-			attribute = args[0]
-			options = args[1] || {}
-			text_field(attribute, options)
-		end
-	end
+	# %w(email_field text_field password_field).each do |form_method|
+	# 	define_method(form_method) do |*args|
+	# 		attribute = args[0]
+	# 		options = args[1] || {}
+	# 		text_field(attribute, options)
+	# 	end
+	# end
 
 	# Include this to generate all html associated with a text field
 	def text_field(attribute, options={})
 		options[:label] ||= attribute # default text field label
 		label_text ||= options.delete(:label).to_s.titleize
 		label_options ||= {}
-		if errors_on?(attribute)
-			#label_options[:class] += ERROR_CLASS
-			options[:class] += " " + ERROR_CLASS
-		end
 		content_tag(:span, class: "row") do
 			content_tag(:div, label(attribute, label_text, label_options), class: "cell") +
-			content_tag(:div, super(attribute, options) + errors_for_field(attribute), class: "cell")
+			content_tag(:div, super(attribute, options), class: "cell") + 
+			content_tag(:div, errors_for_field(attribute, options), class: "cell")
 		end
-		# wrapper tag_options do # creates html tags and classes for block
-		# 	tag_options[:tag] = :div
-		# 	tag_options[:class] = "cell"
-		# 	wrapper options do 
-		# 		label(attribute, label_text, label_options)
-		# 	end
-		# 	wrapper options do 
-		# 		super(attribute, options) + errors_for_field(attribute)
-		# 	end
-		# end
 	end
+
+	def password_field(attribute, options={})
+		options[:label] ||= attribute # default text field label
+		label_text ||= options.delete(:label).to_s.titleize
+		label_options ||= {}
+		content_tag(:span, class: "row") do
+			content_tag(:div, label(attribute, label_text, label_options), class: "cell") +
+			content_tag(:div, super(attribute, options), class: "cell") + 
+			content_tag(:div, errors_for_field(attribute, options), class: "cell")
+		end
+	end
+
+	def email_field(attribute, options={})
+		options[:label] ||= attribute # default text field label
+		label_text ||= options.delete(:label).to_s.titleize
+		label_options ||= {}
+		content_tag(:span, class: "row") do
+			content_tag(:div, label(attribute, label_text, label_options), class: "cell") +
+			content_tag(:div, super(attribute, options), class: "cell") + 
+			content_tag(:div, errors_for_field(attribute, options), class: "cell")
+		end
+	end
+
 
 	# Include this to generate all html associated with a submit button
 	def submit(text, options={})
@@ -78,13 +88,9 @@ class FormBuilder < ActionView::Helpers::FormBuilder
 		# capture encloses the block inside the div, see implementation above
 	end
 
-	def errors_on?(attribute)
-		object.errors[:attribute].size > 0
-	end
-
 	def errors_for_field(attribute, options={})
 		return "" if object.errors[attribute].empty?
-		content_tag(:small, object.errors[attribute].to_sentence, class: ERROR_CLASS)
+		content_tag(:small, object.errors[attribute].to_sentence.downcase.capitalize, class: ERROR_CLASS)
 		# object refers to the object passed into the form
 	end
 end
